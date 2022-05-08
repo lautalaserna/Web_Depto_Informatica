bd0f13b163157e8e9dc9c4d1dc4fa5299baa956d
Lautaro Laserna <lasernalauta@gmail.com>
2022-05-08 00:38:50 -0300

    Iconos front

---
+++ front-end/js/scroll.js
@@ -0,0 +1,37 @@
+let btnInstitucional = document.getElementById("btn-institucional");
+let btnCarrera = document.getElementById("btn-carrera");
+let btnNovedades = document.getElementById("btn-novedades");
+let btnTop = document.getElementById("btn-back-to-top");
+
+window.onscroll = function () {
+    scrollFunction();
+};
+  
+function scrollFunction() {
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        btnTop.style.display = "block";
+    } else {
+        btnTop.style.display = "none";
+    }
+}
+
+btnTop.addEventListener("click", () => {
+    document.body.scrollTop = 400;
+    document.documentElement.scrollTop = 400;
+});
+
+btnInstitucional.addEventListener("click",()=>{
+    document.body.scrollTop = 980;
+    document.documentElement.scrollTop = 980;
+});
+
+btnCarrera.addEventListener("click",()=>{
+    document.body.scrollTop = 1680;
+    document.documentElement.scrollTop = 1680;
+})
+
+btnNovedades.addEventListener("click",()=>{
+    document.body.scrollTop = 2370;
+    document.documentElement.scrollTop = 2370;
+})
+
