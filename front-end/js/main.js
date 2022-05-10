4bdddad932a6577aeaaa47bc283b01edc1cdff7e
julyvjo <julyvjo@gmail.com>
2022-05-10 20:13:26 -0300

    script responsive fixeado

--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,23 +1,32 @@
 // Script
+const VW_LIMIT = 1400;
 window.onload = responsiveSection;
 window.addEventListener("resize", responsiveSection);
 
 function responsiveSection(){
     let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-    let secCarrera = document.getElementById("section-carrera");
-    let imgCarrera = document.getElementById("img-carrera");
-    console.log(vw);
+    let section = document.getElementsByClassName("section");
+    let section_img = document.getElementsByClassName("section-img");
 
-    if(vw < 1400){
-        imgCarrera.style.visibility = "hidden";
-        imgCarrera.classList.remove("col-4");
-        secCarrera.classList.remove("col-8");
-        secCarrera.classList.add("col-12");
+    if(vw < VW_LIMIT){
+        for (var i = 0; i < section.length; i++) {
+            section[i].classList.remove("col-8");
+            section[i].classList.add("col-12");
+        }
+        for (var i = 0; i < section_img.length; i++) {
+            section_img[i].style.display = "none";
+            section_img[i].classList.remove("col-4");
+        }
     }
     else{
-        secCarrera.classList.remove("col-12");
-        secCarrera.classList.add("col-8");
-        imgCarrera.classList.add("col-4");
-        imgCarrera.style.visibility = "visible";
+        for (var i = 0; i < section.length; i++) {
+            section[i].classList.remove("col-12");
+            section[i].classList.add("col-8");
+        }
+
+        for (var i = 0; i < section_img.length; i++) {
+            section_img[i].style.display = "block";
+            section_img[i].classList.add("col-4");
+        }
     }
 }
