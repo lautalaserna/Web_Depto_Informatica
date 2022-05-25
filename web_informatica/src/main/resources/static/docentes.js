7f2a1483cc01d78c90914868da3e0f557202e05e
Lautaro Laserna <lasernalauta@gmail.com>
2022-05-25 11:33:10 -0300

    Docentes terminado

--- web_informatica/src/main/resources/static/docentes.js
+++ web_informatica/src/main/resources/static/docentes.js
@@ -33,16 +33,15 @@ fetchDocentesJSON().then(docentes => {
         let tr = tbody.insertRow(i);
         let td_img = tr.insertCell(0);
         let td_nombre = tr.insertCell(1);
-        let td_email = tr.insertCell(2);
         let td_titulo = tr.insertCell(3);
+        let td_email = tr.insertCell(2);
         let td_asignatura = tr.insertCell(4);
 
-        td_img.innerHTML = '<img class="rounded-circle ms-2" src="' + docentes[i].img_url + '" height="100px" width="100px">';
-        //td_img.innerHTML = '<img width=100 height=100 style="border-radius = 50%" src=' + docente[i].img_url + '></>';
-        td_nombre.innerHTML = docentes[i].nombre_completo;
-        td_titulo.innerHTML = docentes[i].titulo;
-        td_email.innerHTML = docentes[i].email;
-        td_asignatura.innerHTML = docentes[i].asignatura;
+        td_img.innerHTML = `<img class="rounded-circle" src="${docentes[i].img_url}" height="100px" width="100px">`;
+        td_nombre.innerHTML = `<h4 class="py-2">${docentes[i].nombre_completo}</h4>`;
+        td_titulo.innerHTML = `<h4 class="py-2">${docentes[i].titulo}</h4>`;
+        td_email.innerHTML = `<h4 class="py-2">${docentes[i].email}</h4>`;
+        td_asignatura.innerHTML = `<h4 class="py-2">${docentes[i].asignatura}</h4>`;
     }
 
 });
