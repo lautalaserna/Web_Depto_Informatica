8c300afea68e1c37e8364d5b599a15103459ccef
julyvjo <julyvjo@gmail.com>
2022-07-03 16:47:13 -0300

    CRUD graduados funcionando

--- web_informatica/src/main/java/com/example/informatica/admin/AdminGraduadosController.java
+++ web_informatica/src/main/java/com/example/informatica/admin/AdminGraduadosController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import com.example.informatica.carrera.graduados.Graduados;
 import com.example.informatica.carrera.graduados.GraduadosService;
@@ -21,30 +22,55 @@ public class AdminGraduadosController {
 	}
 	
 	//ADMIN
-	@GetMapping("/admin_graduados")
+	@GetMapping("/admin/graduados")
 	public String getAdminGraduados(Model model) {
+		
+		model.addAttribute("headers", List.of("nombre completo","fecha"));
+		model.addAttribute("entidad", "graduados");
 		List<Graduados> graduados = graduadosService.getGraduados();
 		model.addAttribute("graduados", graduados);
 		return "admin/admingraduados";
 	}
-	//CREATE
-	@GetMapping("/admin_graduados_nuevo")
-	public String createGraduado(Model model) {
-		Graduados graduado = new Graduados();
-		model.addAttribute("graduado", graduado);
-		return "admin/create/creategraduados";
+	
+	//CREATE or EDIT
+	@GetMapping("/admin/graduados/editar/{id}")
+	public String editarGraduado(@PathVariable int id, Model model) {
+		//id == 0 significa nuevo
+		if(id == 0) {
+			Graduados graduado = new Graduados();
+			model.addAttribute("graduado", graduado);
+		}else {
+			model.addAttribute("graduado", graduadosService.getGraduado(id).get());
+		}
+		return "admin/forms/formgraduados";
 	}
-	@PostMapping("/admin_graduados_nuevo")
-	public String addGraduado(@ModelAttribute("graduado") Graduados graduado) {
-		graduadosService.addGraduado(graduado);
-		return "redirect:/admin_graduados";
+	@PostMapping("/admin/graduados/{id}")
+	public String actualizarEstudiante(@PathVariable int id, @ModelAttribute("graduado") Graduados graduado) {
+		
+		//id == 0 significa nuevo
+		if(id == 0) {
+			graduadosService.addGraduado(graduado);
+		}else {
+			Graduados graduadoExistente = graduadosService.getGraduado(id).get();
+			graduadoExistente.setId_graduado(id);
+			graduadoExistente.setNombre_completo(graduado.getNombre_completo());
+			graduadoExistente.setFecha(graduado.getFecha());
+			graduadoExistente.setUrl(graduado.getUrl());
+			graduadosService.updateGraduado(graduadoExistente);
+		}
+		return "redirect:/admin/graduados";
 	}
-	//EDIT
-	//@GetMapping()
-	//public String editGraduado() {
-	//}
 	
 	//DELETE
-	//@GetMapping("/admin_graduados_delete")
-	//public 
+	@GetMapping("/admin/graduados/delete/{id}")
+	public String eliminarGraduado(@PathVariable int id) {
+		if(id != 0) {
+			graduadosService.deleteGraduado(id);
+		}
+		return "redirect:/admin/graduados";
+	}
 }
+
+
+
+
--- web_informatica/src/main/java/com/example/informatica/carrera/graduados/GraduadosService.java
+++ web_informatica/src/main/java/com/example/informatica/carrera/graduados/GraduadosService.java
@@ -1,6 +1,7 @@
 package com.example.informatica.carrera.graduados;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,5 +22,17 @@ public class GraduadosService {
 	
 	public void addGraduado(Graduados graduado) {
 		graduadosRepository.save(graduado);
+	}
+	
+	public Optional<Graduados> getGraduado(int id){
+		return graduadosRepository.findById(id);
+	}
+	
+	public void updateGraduado(Graduados graduado) {
+		graduadosRepository.save(graduado);
+	}
+	
+	public void deleteGraduado(int id) {
+		graduadosRepository.deleteById(id);
 	}
 }
